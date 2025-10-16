031ff002485c33ef03d62ca3ce797f1530c7f1b4
Douglas Deodanes <codinwithdev@gmail.com>
2025-10-16 15:11:02 -0600

    Create SubjectRepository.java

---
+++ src/main/java/com/dbz/SGA_asignacion/repository/SubjectRepository.java
@@ -0,0 +1,7 @@
+package com.dbz.SGA_asignacion.repository;
+
+import com.dbz.SGA_asignacion.model.Subject;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SubjectRepository extends JpaRepository<Subject, Long> {
+}
