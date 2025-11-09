c054c9f9f57003c683ee886c142b694686061f74
Douglas Deodanes <codinwithdev@gmail.com>
2025-11-09 17:05:16 -0600

    add : Listar notas de un estudiante

---
+++ src/main/java/com/dbz/SGA_asignacion/controller/GradeController.java
@@ -0,0 +1,34 @@
+package com.dbz.SGA_asignacion.controller;
+
+import com.dbz.SGA_asignacion.response.GradeResponse;
+import com.dbz.SGA_asignacion.services.GradeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1")
+public class GradeController {
+
+    @Autowired
+    private GradeService gradeService;
+
+    @GetMapping("/grades/student/{idStudent}")
+    public ResponseEntity<?>getGradesByStudentId(@PathVariable Long idStudent){
+        List<?> gradesStudent = gradeService.getAllGradesByIdStudent(idStudent);
+        return ResponseEntity.ok(new GradeResponse("Success", gradesStudent));
+
+    }
+
+    @GetMapping("/grades/course/{idCourse}")
+    public ResponseEntity<?>getGradesByCourseId(@PathVariable Long idCourse){
+    List<?> gradesCourse = gradeService.getAllGradesByIdCourse(idCourse);
+        return ResponseEntity.ok(new GradeResponse("Success", gradesCourse));
+    }
+
+}
