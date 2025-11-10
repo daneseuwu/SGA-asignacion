918ee55ebdaf5ea4e00b50b46f5488a3ada9c6ab
Douglas Deodanes <codinwithdev@gmail.com>
2025-11-09 18:26:27 -0600

    Update GradeService.java

--- src/main/java/com/dbz/SGA_asignacion/services/GradeService.java
+++ src/main/java/com/dbz/SGA_asignacion/services/GradeService.java
@@ -1,10 +1,11 @@
 package com.dbz.SGA_asignacion.services;
 
+import com.dbz.SGA_asignacion.dto.GradeDTO;
 import com.dbz.SGA_asignacion.model.Grade;
 
 import java.util.List;
 
 public interface GradeService {
-    List<Grade> getAllGradesByIdStudent(Long idStudent);
-    List<Grade> getAllGradesByIdCourse(Long idCourse);
+    List<GradeDTO> getAllGradesByIdStudent(Long idStudent);
+    List<GradeDTO> getAllGradesByIdCourse(Long idCourse);
 }
